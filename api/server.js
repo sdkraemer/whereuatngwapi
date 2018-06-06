1a7e8da0762645ee02e6917019e33d9bcee77049
Scott Kraemer <skraemer@CLG.Local>
2018-06-06 15:39:38 -0700

    added config

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 console.log("/api");
 var express = require("express");
 var app = express();
+var config = require("config");
 var jwt = require("express-jwt");
 var jwks = require("jwks-rsa");
 var dotenv = require("dotenv");
@@ -22,16 +23,17 @@ app.use(
 //    secret: process.env.AUTH0_CLIENT_SECRET,
 //    audience: process.env.AUTH0_CLIENT_AUDIENCE
 //  });
+var auth0Config = config.get("auth0");
 
 var jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: "https://feeldaburn.auth0.com/.well-known/jwks.json"
+    jwksUri: `https://${auth0Config.CLIENT_DOMAIN}/.well-known/jwks.json`
   }),
-  audience: "http://localhost/api",
-  issuer: "https://feeldaburn.auth0.com/",
+  audience: auth0Config.AUTH0_AUDIENCE,
+  issuer: `https://${auth0Config.CLIENT_DOMAIN}/`,
   algorithms: ["RS256"]
 });
 

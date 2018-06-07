51a253b3244e084f1280c6501d00594fb9ac1ff3
Scott Kraemer <skraemer@CLG.Local>
2018-06-06 21:10:16 -0700

    screwed up auth0 url

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: "http://165.227.57.162/api",
   auth0: {
-    audience: "http://165.227.57.162.net/api",
-    redirect: "http://165.227.57.162.net/auth0callback"
+    audience: "http://165.227.57.162/api",
+    redirect: "http://165.227.57.162/auth0callback"
   }
 };
